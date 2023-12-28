4e4dcf96efbe08cf89eaa86baa70019c7cc6cfda
miusarname <oscarm.alvarezg@gmail.com>
2023-12-28 14:27:09 -0500

    feat: :tada: Begin project

---
+++ Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by user252869 on 28/12/23.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
